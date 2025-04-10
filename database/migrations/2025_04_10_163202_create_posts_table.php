7d12fc71e439b3d1dcbee770f67c9ac3c34533e5
Hasbi Mizan <hasbimizan@student.ub.ac.id>
2025-04-10 23:46:52 +0700

    Restore Laravel 12

---
+++ database/migrations/2025_04_10_163202_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->string('id', 18)->primary();
+            $table->string('user_id', 8);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description', 3000);
+            $table->string('photo', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
